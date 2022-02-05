216e8b3a114af0ac373141b6e72e951bd761e52a
Gregory Grubbs <gregory@dynapse.com>
2022-02-04 17:30:31 -0700

    Add Guix channels definition and update mechanism to dotfiles

---
+++ dotfiles/.config/guix/channels.scm
@@ -0,0 +1,25 @@
+(list (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+         "4c0b9a86521a6d06c895b41e62c254da83feff7a"))
+      (channel
+       (name 'flat)
+       (url "https://github.com/flatwhatson/guix-channel.git")
+       (introduction
+        (make-channel-introduction
+         "33f86a4b48205c0dc19d7c036c85393f0766f806"
+         (openpgp-fingerprint
+          "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "3e62af6f3b2efcf3181094be79da237ac9208d46")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
