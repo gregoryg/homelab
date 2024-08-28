3b3fafc963db83fd50a60932424a9cd8c9ee2eef
Gregory Grubbs <gregory@dynapse.com>
2024-08-27 19:41:59 -0600

    last Stardog image changes I'll ever make (?)

--- graph/stardog/stardog-images/build.sh
+++ graph/stardog/stardog-images/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-     # --build-arg http_proxy=http://172.16.17.5:3128 \
-     # --build-arg https_proxy=http://172.16.17.5:3128 \
+# --build-arg http_proxy=http://172.16.17.5:3128 \
+# --build-arg https_proxy=http://172.16.17.5:3128 \
 time docker build \
-     --tag gregoryg/dbnetutils:latest \
-     --tag gregoryg/dbnetutils:8.2 .
+  -f Dockerfile-from-base-image \
+  --tag gregoryg/dbnetutils:latest \
+  --tag gregoryg/dbnetutils:9.2.0 \
+  .
